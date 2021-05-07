3f169d0a1e690842952ed126e4cbaf17e2da4570
Zachary Sweet <ztsc97@mst.edu>
2021-05-06 21:05:16 -0500

    first commit: Added generated project

---
+++ src/test/java/com/example/Maintain/MaintainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Maintain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
